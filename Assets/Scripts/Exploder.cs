2323a8d778c16ec46963a8835a6d434456972826
Daniel <Sychkov.Daniil@yandex.ru>
2024-09-04 14:40:21 +0300

    Initial commit

---
+++ Assets/Scripts/Exploder.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Exploder : MonoBehaviour
+{
+    private const float MaxProbability = 1f;
+    private const int ProbabilityDecreaseRatio = 2;
+
+    private float _probability = MaxProbability;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hitInfo))
+            {
+                if (hitInfo.collider.TryGetComponent(out ExplosiveCube cube))
+                {
+                    cube.Explode(_probability);
+                    _probability /= ProbabilityDecreaseRatio;
+                }
+            }
+        }
+    }
+}
